35d9ea055df2bf74f0e3dc0611220963e53d4454
Cristian Ruiz <cristiansanta13@hotmail.com>
2023-03-02 01:33:35 -0500

    Upload

---
+++ src/main/java/com/persistence/persistenceH2/PersistenceH2Application.java
@@ -0,0 +1,13 @@
+package com.persistence.persistenceH2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistenceH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersistenceH2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/persistence/persistenceH2/PersistenceH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.persistence.persistenceH2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersistenceH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
